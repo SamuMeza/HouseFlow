32f147b2cdf226dd345b1a9c057f571cac03aca5
Samuel <samu12meza@gmail.com>
2026-03-12 15:07:58 -0400

    Importando de correcta manera los componentes de navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainLayout } from './components/organisms/MainLayout'
-import { Home } from './pages/Home'
-import { Dashboard } from './pages/Dashboard'
-import { History } from './pages/History'
-import { Teleprompter } from './pages/Teleprompter'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import History from './pages/History'
+import Teleprompter from './pages/Teleprompter'
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="history" element={<History />} />
-            <Route path="teleprompter" element={<Teleprompter />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="history" element={<History />} />
+          <Route path="teleprompter" element={<Teleprompter />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

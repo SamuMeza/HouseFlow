1c979d25d1b005f1d7868c6a800a7983bcf8ab9f
Samuel <samu12meza@gmail.com>
2026-03-14 13:22:48 -0400

    Agregado nuevas animaciones para el Loader, y tipado agregado al archivo para hacerlos objetos inmutables

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,10 +1,12 @@
 // src/utils/animations.ts
 
+//as const es para que el objeto sea inmutable
+
 export const fadeInUp = {
     initial: { opacity: 0, y: 20 },
     animate: { opacity: 1, y: 0 },
     transition: { duration: 0.8, ease: "easeOut" }
-};
+} as const;
 
 export const staggerContainer = {
     initial: { opacity: 0 },
@@ -14,4 +16,20 @@ export const staggerContainer = {
             staggerChildren: 0.2 // Esto hace que los hijos aparezcan uno tras otro
         }
     }
-};
+} as const;
+
+export const fadeAnimation = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.3 }
+} as const;
+
+export const infiniteRotate = {
+    animate: { rotate: 360 },
+    transition: {
+        repeat: Infinity,
+        duration: 2,
+        ease: "linear"
+    }
+} as const;

65bb6b42741e8172d709d0260b91f228f7d753fd
Samuel <samu12meza@gmail.com>
2026-03-18 22:07:50 -0400

    Simple agregado del enlace a github del creador

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="container">
                 <p><Copyright size={20} /> {new Date().getFullYear()} HouseFlow. Todos los derechos reservados.</p>
+                <p>Creado por <a href="https://github.com/SamuMeza" target="_blank" rel="noopener noreferrer">Samuel Meza</a></p>
             </div>
         </footer>
     )
